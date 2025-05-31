3736442bd994b878f2e6087d416c48d4a96feb03
Shinya Harada <haradasn01@gmail.com>
2025-05-31 15:09:09 +0900

    tagを追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,9 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_prefix.cidr_block
   enable_dns_hostnames = true
   enable_dns_support   = true
+  tags = {
+    Name = var.vpc_prefix.prefix
+  }
 }
 
 variable "vpc_prefix" {
